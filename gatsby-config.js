455a70b0d90c2ef7bba74eee3591b78eabcf7759
favi <favi@users.noreply.github.com>
2021-09-05 11:25:56 +0100

    layout work

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
+        path: `${__dirname}/src/components`,
       },
     },
     {
